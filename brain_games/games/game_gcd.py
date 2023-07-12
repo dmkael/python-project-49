88d705caa6e23c23f3dc9f3b03cd56b80cd74e3a
dmkael <tanmaar@gmail.com>
2023-07-12 15:29:17 +0300

    MODIFY game_gcd.py for less cognitive complexity

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,8 +1,7 @@
 import random
 
 
-def gcd():
-    question = "Find the greatest common divisor of given numbers."
+def get_numbers():
     num1 = random.randint(2, 50)
     divs1 = []
     for div1 in range(1, int(num1 // 2 + 1)):
@@ -11,12 +10,18 @@ def gcd():
     num2 = divs1[random.randint(0, len(divs1) - 1)] * random.randint(2, 10)
     while num2 == num1:
         num2 = divs1[random.randint(0, len(divs1) - 1)] * random.randint(2, 10)
+    return num1, num2
+
+
+def gcd():
+    question = "Find the greatest common divisor of given numbers."
+    num1, num2 = get_numbers()
     expression = f"{num1} {num2}"
-    result = []
+    divs = []
     for common_div in range(1, min(num1, num2) + 1):
         if num1 % common_div == 0 and num2 % common_div == 0:
-            result.append(common_div)
-    result = str(max(result))
+            divs.append(common_div)
+    result = str(max(divs))
     return question, expression, result
 
 
