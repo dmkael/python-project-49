6fbdffde916c51695fab33d0ecc7396556dcaa2c
dmkael <tanmaar@gmail.com>
2023-07-18 17:01:30 +0300

    MODIFY rename game_progression functions attribute

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -3,19 +3,19 @@ import random
 TASK = "What number is missing in the progression?"
 
 
-def generate_progression(prog_length, progressor, start_num):
+def generate_progression(prog_length, step, start_num):
     progression = [start_num]
     for index in range(1, prog_length):
-        start_num += progressor
+        start_num += step
         progression.append(start_num)
     return progression
 
 
 def run_progression():
     prog_length = random.randint(9, 11)
-    progressor = random.randint(2, 10)
+    step = random.randint(2, 10)
     start_num = random.randint(0, 50)
-    progression = generate_progression(prog_length, progressor, start_num)
+    progression = generate_progression(prog_length, step, start_num)
 
     result_index = random.randint(0, prog_length - 1)
     result = progression[result_index]
